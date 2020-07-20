d242cf470927554fbd799a6a4ae5db63377e78e2
deatiger <deatiger1492@gmail.com>
2020-07-21 00:02:22 +0900

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    // モード値を production に設定すると最適化された状態で、
+    // development に設定するとソースマップ有効でJSファイルが出力される
+    mode: 'development',
+    entry: 'src/index.ts',
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+            },
+        ],
+    },
+    resolve: {
+        extensions: [
+            '.ts', '.js', '.tsx', '.jsx'
+        ],
+    },
+};
