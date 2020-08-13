ad4212531a3784efcd747fbde688f1a1bac6449c
deatiger <deatiger1492@gmail.com>
2020-08-13 19:26:07 +0900

    Added BaseButton, PrimaryButton, and SecondaryButton components. Also create Flexbox

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -3,14 +3,35 @@ import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
 import { withKnobs } from "@storybook/addon-knobs";
 
-import {PrimaryButton} from "./index";
+import {PrimaryButton, SecondaryButton} from "./index";
+import {GridRow} from '../Grid'
 
 const components = storiesOf("Components", module);
 components
-    .addDecorator(withKnobs)
-    .addDecorator(withInfo({ inline: true }))
-    .add("Button", () => (
+  .addDecorator(withKnobs)
+  .addDecorator(withInfo({ inline: true }))
+  .add("Button", () => (
+    <section>
+      <GridRow wrapStyle={"nowrap"}>
         <PrimaryButton
-            label={"登録する"}
+          label={"Primary Default"}
+          size={"default"}
         />
-    ));
+        <SecondaryButton
+          label={"Secondary Default"}
+          size={"default"}
+        />
+      </GridRow>
+      <GridRow wrapStyle={"nowrap"}>
+        <PrimaryButton
+          label={"Primary Small"}
+          onClick={() => console.log("Primary!")}
+          size={"small"}
+        />
+        <SecondaryButton
+          label={"Secondary Small"}
+          size={"small"}
+        />
+      </GridRow>
+    </section>
+  ))
--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -1,1 +1,2 @@
-export {default as PrimaryButton} from './PrimaryButton'
+export {PrimaryButton} from './PrimaryButton'
+export {SecondaryButton} from './SecondaryButton'
